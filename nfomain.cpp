0b8a780f7ea624bb781a33513ffa2dd2edcd72a4
Fabien Brisset <fbrisset@geexbox.org>
2010-02-23 08:49:33 +0100

    adding nfo-editor under LGPL (check with Qt licence done)

--- nfomain.cpp
+++ nfomain.cpp
@@ -1,3 +1,24 @@
+/*
+ * GeeXboX NFO editor: GUI for media scanner application.
+ * Copyright (C) 2009 Fabien Brisset <fbrisset@geexbox.org>
+ *
+ * This file is part of nfo-editor.
+ *
+ * nfo-editor is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * nfo-editor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libvalhalla; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 #include "nfomain.h"
 
 NfoMain::NfoMain()
